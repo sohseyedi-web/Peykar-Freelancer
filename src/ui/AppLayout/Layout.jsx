c6cdd21df235232279c10e2a569a97f15c405d50
sohseyedi <sohseyedi@gmail.com>
2023-12-12 15:35:44 +0330

    design sidebar

--- src/ui/AppLayout/Layout.jsx
+++ src/ui/AppLayout/Layout.jsx
@@ -7,8 +7,8 @@ const Layout = () => {
     <section dir="rtl" className="grid h-screen grid-cols-[15rem_1fr] grid-rows-[auto_1fr]">
       <Header />
       <Sidebar />
-      <div className="overflow-y-auto p-8">
-        <div className="mx-auto max-w-screen-xl">
+      <div className="overflow-y-auto p-4 px-8">
+        <div className="mx-auto lg:max-w-screen-xl w-full">
           <Outlet />
         </div>
       </div>
